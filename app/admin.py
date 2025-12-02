48d31c6bfa2f1a270983e9670a39b59b8f152458
unknown <pheydari33@yahoo.com>
2025-12-02 20:19:01 +0330

    Customize Document and Query admin display

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Document, Query
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'short_content', 'tags')
+    search_fields = ('title', 'content', 'tags')
+    list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at',)
+
+    def short_content(self, obj):
+        return obj.content[:100] + "..." if len(obj.content) > 100 else obj.content
+    short_content.short_description = "short content"
+
+@admin.register(Query)
+class QueryAdmin(admin.ModelAdmin):
+    list_display = ('question', 'created_at', 'has_answer')
+    search_fields = ('question', 'answer')
+    list_filter = ('created_at')
+    ordering = ('-created_at')
+    readonly_fields = ('created_at')
+
+    @admin.display(boolean=True, description="has answer?")
+    def has_answer(self, obj):
+        return bool(obj.answer)
